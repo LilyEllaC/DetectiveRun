a1bca35994dd6aefc696baa0f93253b27ae990d8
Llily Ella C <lilyellacallard@gmail.com>
2026-01-06 10:52:25 -0500

    basic starting stuff

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame 
+import constants
+import utility
+import asyncio
+
+#running
+running=True
+
+
+
+async def main():
+    global running
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+        #ending stuff
+        pygame.display.flip()
+        constants.clock.tick(constants.FPS)
+        await asyncio.sleep(0)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    pygame.quit()
---
+++ utility.py
@@ -0,0 +1,20 @@
+import constants
+
+# helpful pushing text to screen function
+def toScreen(words, font, colour, x, y):
+    text = font.render(words, True, colour)
+    textRect = text.get_rect()
+    textRect.center = (x, y)
+    constants.screen.blit(text, textRect)
+
+
+# versions to push more than 1 line
+def toScreen2(words1, words2, font, colour, x, y):
+    toScreen(words1, font, colour, x, y - font.get_height() // 2)
+    toScreen(words2, font, colour, x, y + font.get_height() // 2)
+
+
+def toScreen3(words1, words2, words3, font, colour, x, y):
+    toScreen(words1, font, colour, x, y - font.get_height())
+    toScreen(words2, font, colour, x, y)
+    toScreen(words3, font, colour, x, y + font.get_height())
